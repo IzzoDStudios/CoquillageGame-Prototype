87e0a7241e5f54c880437a4f97b325d6941f2e2e
Mario Melendez <mariomelendez099@gmail.com>
2024-05-18 16:21:04 -0600

    None

--- game/src/universalVariables.py
+++ game/src/universalVariables.py
@@ -6,9 +6,13 @@ pygame.init()
 
 # Screen Universal Variables.
 SIZESCREEN = (1980, 720) # Size of the Screen .
+
 WIDTHSCREEN, HEIGHTSCREEN = SIZESCREEN # Distribute the Screen size to Width and Height of the Screen.
+
 screenName = "Coquillage Project" # That´s the Name who the Screen Will Have.
+
 screen = pygame.display.set_mode(SIZESCREEN, pygame.FULLSCREEN) # Here I Only Init my Screen Variable.
+
 pygame.display.set_caption(screenName) # Here i Put the Name Previously Init.
 
 # Fps Variable
